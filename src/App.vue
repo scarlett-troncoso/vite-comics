d0ca98e0d7e58f790832cf6c642d3bb4220cd3bc
scarlett-troncoso <scar.labussieri@gmail.com>
2024-02-01 10:26:10 +0100

    inital commit and initial tags

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script >
+export default {
+  name: 'App',
+}
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div>
+        <img src="/img/dc-logo.png" alt="">
+      </div>
+
+      <nav>
+        <ul>
+          <li><a href="">CHARACTERS</a></li>
+          <li><a href="">COMICS</a></li>
+          <li><a href="">MOVIES</a></li>
+          <li><a href="">TV</a></li>
+          <li><a href="">GAMES</a></li>
+          <li><a href="">COLLECTIBLES</a></li>
+          <li><a href="">VIDEOS</a></li>
+          <li><a href="">FANS</a></li>
+          <li><a href="">NEWS</a></li>
+          <li><a href="">SHOP</a></li>
+        </ul>
+      </nav>
+
+    </div>
+  </header>
+
+  <main></main>
+
+  <footer></footer>
+</template>
+
+<style></style>
