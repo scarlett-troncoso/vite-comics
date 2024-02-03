1a12de8b78e4fa6a6c4720fc3546cd15d69f644c
scarlett-troncoso <scar.labussieri@gmail.com>
2024-02-03 18:25:08 +0100

    add component card comics main

---
+++ src/components/ComicsCards.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: 'ComicsCards.vue',
+    props: {
+        comic: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="cont-img">
+            <img :src="comic.thumb" alt="">
+        </div>
+        <h5>{{ comic.series }}</h5>
+        <span>{{ comic.price }}</span>
+    </div>
+</template>
+
+
+
+<style scoped>
+.card {
+
+    & .cont-img {
+        width: 150px;
+
+        & img {
+            max-width: 100%;
+        }
+
+    }
+}
+</style>
